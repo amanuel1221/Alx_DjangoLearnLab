17b6e0557f52b3ace45bda99da39f8c27d05e67b
Amanuel <beginningoflife2324@gmail.com>
2025-09-09 23:17:36 -0700

    fa

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("admin-view/", admin_view, name="admin_view"),
     path("librarian-view/", librarian_view, name="librarian_view"),
     path("member-view/", member_view, name="member_view"),
-    path("books/add/", add_book, name="add_book"),
-    path("books/<int:pk>/edit/", edit_book, name="edit_book"),
+    path("add_book/", add_book, name="add_book"),
+    path("edit_book/<int:pk>/", edit_book, name="edit_book"),
     path("books/<int:pk>/delete/", delete_book, name="delete_book"),
 ]
